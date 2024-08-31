9e5c732d042fa9233b18a8bde2ab195ffc7d9255
Leonardo Lima <oleonardolima@users.noreply.github.com>
2024-08-30 23:45:12 -0300

    feat(descriptors): generate new descriptors or used specified ones

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,27 @@ use bdk_wallet::{
 
 const NETWORK: Network = Network::Signet;
 
+const EXTERNAL_DESCRIPTOR: &str = "";
+const INTERNAL_DESCRIPTOR: &str = "";
+
 fn main() {
+    let (external_descriptor, internal_descriptor) =
+        if EXTERNAL_DESCRIPTOR.is_empty() || INTERNAL_DESCRIPTOR.is_empty() {
+            create_descriptors()
+        } else {
+            (
+                EXTERNAL_DESCRIPTOR.to_string(),
+                INTERNAL_DESCRIPTOR.to_string(),
+            )
+        };
+
+    println!("\n!!!! PLEASE CREATE A BACKUP OF YOUR DESCRIPTORS BELOW !!!!\n");
+    println!("EXTERNAL DESCRIPTOR: {:?}\n", external_descriptor);
+    println!("INTERNAL DESCRIPTOR: {:?}", internal_descriptor);
+    println!("\n!!!! PLEASE CREATE A BACKUP OF YOUR DESCRIPTORS ABOVE !!!!\n");
+}
+
+fn create_descriptors() -> (String, String) {
     // Create your initial seed value
     let mut seed: [u8; 32] = [0u8; 32];
     rand::thread_rng().fill_bytes(&mut seed);
@@ -33,4 +53,9 @@ fn main() {
         change_descriptor.to_string_with_secret(&change_keymap)
     );
     println!("\n!!!! PLEASE CREATE A BACKUP OF YOUR DESCRIPTORS ABOVE !!!!\n");
+
+    (
+        descriptor.to_string_with_secret(&keymap),
+        change_descriptor.to_string_with_secret(&change_keymap),
+    )
 }
