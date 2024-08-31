bc8c2b94bd4c885fbbccb6abac5a014a5085d910
Leonardo Lima <oleonardolima@users.noreply.github.com>
2024-08-30 23:56:14 -0300

    feat(wallet): create wallet from given descriptors

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use bdk_wallet::{
         Network,
     },
     template::{Bip86, DescriptorTemplate},
-    KeychainKind,
+    AddressInfo, KeychainKind, Wallet,
 };
 
 const NETWORK: Network = Network::Signet;
@@ -18,16 +18,38 @@ fn main() {
         if EXTERNAL_DESCRIPTOR.is_empty() || INTERNAL_DESCRIPTOR.is_empty() {
             create_descriptors()
         } else {
+            println!("\n!!!! PLEASE CREATE A BACKUP OF YOUR DESCRIPTORS BELOW !!!!\n");
+            println!("EXTERNAL DESCRIPTOR: {:?}\n", EXTERNAL_DESCRIPTOR);
+            println!("INTERNAL DESCRIPTOR: {:?}", INTERNAL_DESCRIPTOR);
+            println!("\n!!!! PLEASE CREATE A BACKUP OF YOUR DESCRIPTORS ABOVE !!!!\n");
+
             (
                 EXTERNAL_DESCRIPTOR.to_string(),
                 INTERNAL_DESCRIPTOR.to_string(),
             )
         };
 
-    println!("\n!!!! PLEASE CREATE A BACKUP OF YOUR DESCRIPTORS BELOW !!!!\n");
-    println!("EXTERNAL DESCRIPTOR: {:?}\n", external_descriptor);
-    println!("INTERNAL DESCRIPTOR: {:?}", internal_descriptor);
-    println!("\n!!!! PLEASE CREATE A BACKUP OF YOUR DESCRIPTORS ABOVE !!!!\n");
+    // Create BDK Wallet from both (receive) external descriptor and (change) internal descriptor
+    let mut wallet: Wallet = Wallet::create(external_descriptor, internal_descriptor)
+        .network(NETWORK)
+        .create_wallet_no_persist()
+        .unwrap();
+
+    // Reveal an address from (receive) external keychain
+    let receiving_address: AddressInfo = wallet.reveal_next_address(KeychainKind::External);
+
+    // Reveal an address from (change) internal keychain
+    let change_address: AddressInfo = wallet.reveal_next_address(KeychainKind::Internal);
+
+    println!(
+        "REVEALED RECEIVE (EXTERNAL) ADDRESS {} @ INDEX {}",
+        receiving_address.address, receiving_address.index
+    );
+
+    println!(
+        "REVEALED CHANGE (INTERNAL) ADDRESS {} @ INDEX {}",
+        change_address.address, change_address.index
+    );
 }
 
 fn create_descriptors() -> (String, String) {
